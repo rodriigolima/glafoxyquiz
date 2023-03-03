dfcc6b5075fa3ee68da870137def721ab85e3a02
rmls <rodrigomoreiralima@hotmail.com>
2023-03-02 23:34:55 -0300

    fix: fix bug method delete

--- src/main/java/br/com/glafoxyquiz/api/controller/QuestionController.java
+++ src/main/java/br/com/glafoxyquiz/api/controller/QuestionController.java
@@ -6,8 +6,10 @@ import br.com.glafoxyquiz.domain.repository.QuestionRepository;
 import br.com.glafoxyquiz.domain.service.RegisterQuestionService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
